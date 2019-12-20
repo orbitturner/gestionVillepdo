239697370a93eb6831bb32d87c82c890a2f454b9
orbitturner <orbitturner@gmail.com>
2019-12-20 21:41:45 +0000

    Uploading from The BioLab

---
+++ src/routes/dir.php
@@ -0,0 +1,14 @@
+<?php
+
+function getProjectRoot(){
+    return "/lpgl_projects/gestionVillepdo/";
+}
+function getProjectPath(){
+    return "/lpgl_projects/gestionVillepdo/src/";
+}
+
+function getProjectPublic(){
+    return "/lpgl_projects/gestionVillepdo/public/";
+}
+
+?>
